7e2f2dc225b54770218bdacd89f187a81b175914
mtdamir <amir.motavas79@gmail.com>
2023-09-16 13:03:18 +0430

    refactor handler pkg and implementing  the encoder

---
+++ product-api/handlers/products.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"main/data"
+	"net/http"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	lp := data.GetProducts()
+	d, err := json.Marshal(lp)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+
+	rw.Write(d)
+}
--- product-api/main.go
+++ product-api/main.go
@@ -14,12 +14,10 @@ func main() {
 
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
-	hh := handlers.NewHello(l)
-	gh := handlers.NewGoodbye(l)
+	ph := handlers.NewProducts(l)
 
 	sm := http.NewServeMux()
-	sm.Handle("/", hh)
-	sm.Handle("/goodbye", gh)
+	sm.Handle("/", ph)
 
 	s := &http.Server{
 		Addr:         ":8080",
@@ -30,6 +28,7 @@ func main() {
 	}
 
 	go func() {
+		l.Println("Starting server on port 8080")
 		err := s.ListenAndServe()
 		if err != nil {
 			l.Fatal(err)
